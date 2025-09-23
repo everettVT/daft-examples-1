c459b82aaf03f19720b66710da2d10378f2cfe4c
everettVT <everett@vangelis.tech>
2025-09-23 14:48:02 -0700

    add clean example template

---
+++ _template/main.py
@@ -0,0 +1,23 @@
+import daft 
+from daft import col
+
+
+# Helper Functions and UDFs
+@daft.func()
+def foo(str: str) -> str:
+    return str + " bar"
+
+# Config
+uri = "hf://datasets/Eventual-Inc/sample-files/papers/*.pdf"
+row_limit = 10
+
+# Dataframe Operations
+df = (
+    daft.from_glob_path(uri) 
+    .with_column("foobar_path", foo(col("path"))) # Apply a function to the path column
+    .with_column("document", col("path").url.download()) # Download documents
+    .limit(row_limit)
+)
+
+# Display the first 8 results
+df.show()
